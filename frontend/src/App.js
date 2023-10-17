902e03c461ffec94dc862ca4c26b653a0aa96f29
Kaash Desai <138083663+kaashdesai123@users.noreply.github.com>
2023-10-16 21:26:35 -0700

    Create App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Editor from './components/Editor';
+
+function App() {
+    return (
+        <div className="App">
+            <h1>Real-time Collaborative Editor</h1>
+            <Editor />
+        </div>
+    );
+}
+
+export default App;
